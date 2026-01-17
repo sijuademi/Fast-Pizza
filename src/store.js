f977c4f24d4c5cc541e7a669feea708ab9a9a7d7
Ibrahim Adebayo <sijuadebayo@gmail.com>
2026-01-18 00:52:24 +0100

    Fixed bug in userSlice

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./features/user/UserSlice";
+import userReducer from "./features/user/userSlice";
 import cartReducer from "./features/cart/cartSlice";
 
 const store = configureStore({
